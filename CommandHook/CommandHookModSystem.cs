d1e9f6ab237082655d5d5623832f609c47a72317
anth64 <anth64@protonmail.com>
2025-12-02 21:21:13 +0100

    Initial commit.

---
+++ CommandHook/CommandHookModSystem.cs
@@ -0,0 +1,28 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Common;
+
+namespace CommandHook;
+
+public class CommandHookModSystem : ModSystem
+{
+
+    // Called on server and client
+    // Useful for registering block/entity classes on both sides
+    public override void Start(ICoreAPI api)
+    {
+        Mod.Logger.Notification("Hello from template mod: " + api.Side);
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("commandhook:hello"));
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("commandhook:hello"));
+    }
+
+}
